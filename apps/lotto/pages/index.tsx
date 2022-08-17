d568ca0a723ad8503ef0740bbed761ff6b80daff
niulwinr87 <niulwinrios87@melonn.com>
2022-08-16 21:57:48 -0500

    new pages

--- apps/lotto/pages/index.tsx
+++ apps/lotto/pages/index.tsx
@@ -1,19 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { HomeContainer } from '../view/home/home.container'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { SuperLotto } from '../view';
+// import { HomeContainer } from '../view/home/home.container'
 
-const Index: NextPage = props => {
-    return (
-        <>
-            <Head>
-                <title>Try luck</title>
-                <meta name='description' content='Lucky app' />
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
-            <HomeContainer {...props} />
+const Index: NextPage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Try luck</title>
+        <meta name="description" content="Lucky app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <HomeContainer {...props} /> */}
+      <SuperLotto />;
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Index
+export default Index;
